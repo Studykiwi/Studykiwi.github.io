27e06b079f4bd8ba59179013a8b5906568a18694
AnthonyHawke4 <anthonyjameshawke@gmail.com>
2023-10-06 10:11:12 +1300

    added footers to all pages, with email and donos

---
+++ footerscript.js
@@ -0,0 +1,14 @@
+
+function adjustFooterPosition() {
+    const contentHeight = document.body.clientHeight;
+    const viewportHeight = window.innerHeight;
+
+    if (contentHeight < viewportHeight) {
+        document.querySelector('footer').style.position = 'fixed';
+    } else {
+        document.querySelector('footer').style.position = 'relative';
+    }
+}
+
+window.addEventListener('DOMContentLoaded', adjustFooterPosition);
+window.addEventListener('resize', adjustFooterPosition);
